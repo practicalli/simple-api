d450ce9a11d1045656aa98c1ff296fe0f1af9517
John Stevenson <john@jr0cket.co.uk>
2019-11-08 22:36:20 +0000

    Add simple server that returns a string for any request
    Required the httpkit server namespace
    
    Defined a handler function that takes all requests and returns a ring response
    object with status okay (200) and a hello world string as the body of the
    response.
    
    Defined a server to listen for all http requests and pass them to the handler to
    process a response.
    
    Defnined a simple function to call the server with a specific port
    number (hacky).

--- src/practicalli/simple_api.clj
+++ src/practicalli/simple_api.clj
@@ -1,7 +1,24 @@
 (ns practicalli.simple-api
-  (:gen-class))
+  (:gen-class)
+  (:require [org.httpkit.server :as server]))
+
+(defn handler
+  "A function that handles all requests from the server.
+  `req` is a ring request map"
+  [req]
+  {:status  200
+   :body    "Hello Clojure Server world!"
+   :headers {}})
+
+
+(defn server
+  "A ring-based server listening to all http requests
+  port is an Integer greater than 128"
+  [port]
+  (server/run-server handler {:port port}))
+
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  "Hacky server start function."
+  []
+  (server 8000))
