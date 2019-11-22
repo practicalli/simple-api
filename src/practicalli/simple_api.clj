a4247a6f4c357a511e16b84c3a77f9fa8d653e9e
Practicalli <john@practical.li>
2019-11-22 17:36:08 +0000

    Update: functions to start / stop the server
    Define an atom that will contain a reference to the server when created and be
    nil when no server is running.
    
    To stop the server, a timeout keyword and value is sent to the server reference
    in the atom.

--- src/practicalli/simple_api.clj
+++ src/practicalli/simple_api.clj
@@ -10,15 +10,50 @@
    :body    "Hello Clojure Server world!"
    :headers {}})
 
+;; System
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn server
-  "A ring-based server listening to all http requests
-  port is an Integer greater than 128"
-  [port]
-  (server/run-server handler {:port port}))
+;; Define a name to represent our server
+;; This holds the state of our application,
+;; server is running and can receive further commands
+;; or our server is not running and the value is nil
+(def server (atom nil))
 
 
-#_(defn -main
-    "Hacky server start function."
-    []
-    (server 8000))
+
+(defn stop-server
+  "Gracefully shutdown the server, waiting 100ms"
+  []
+  (when-not (nil? @server)
+    (println "INFO: Gracefully shutting down server...")
+    (@server :timeout 100)
+    (reset! server nil)))
+
+#_(defn -main [& args]
+    ;; The #' is useful when you want to hot-reload code
+    ;; You may want to take a look: https://github.com/clojure/tools.namespace
+    ;; and http://http-kit.org/migration.html#reload
+    (reset! server (server/run-server #'handler {:port (or (first args) 8080)})))
+
+
+(defn -main
+  "Start a httpkit server with a specific port
+  #' enables hot-reload of the handler function and anything that code calls"
+  [& {:keys [ip port]
+      :or   {ip   "0.0.0.0"
+             port 8000}}]
+  (println "INFO: Starting httpkit server - listening on: " (str "http://" ip ":" port))
+  (reset! server (server/run-server #'handler {:port port})))
+
+
+
+
+(def server-config
+  {:ip-address "0.0.0.0"
+   :port       8080})
+
+(defn optional-keys [& {:keys [ip-address port]
+                        :or   {port (:port server-config) ip-address (:ip-address server-config)}}]
+  (str "Port: " port ", address " ip-address))
+
+
